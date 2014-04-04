1d3e80e7a6cbc5c4750876d9f9dd977436906056
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-04-04 18:00:39 +0200

    Add Butterknife for more structure

--- app/src/main/java/de/sliceanddice/maryandpaul/sample/MainActivity.java
+++ app/src/main/java/de/sliceanddice/maryandpaul/sample/MainActivity.java
@@ -2,8 +2,6 @@ package de.sliceanddice.maryandpaul.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -12,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.sliceanddice.maryandpaul.lib.ShopApiClient;
 import de.sliceanddice.maryandpaul.lib.enums.Direction;
 import de.sliceanddice.maryandpaul.lib.enums.Endpoint;
@@ -38,116 +38,96 @@ import de.sliceanddice.maryandpaul.lib.request.ProductSearchRequest;
 
 public class MainActivity extends Activity {
 
-    public static final String APP_ID = "110";
-    public static final String APP_PASSWORD = "ed8272cc4d993378f595d112915920bb";
+    private static final String APP_ID = "110";
+    private static final String APP_PASSWORD = "ed8272cc4d993378f595d112915920bb";
+    
+    private ShopApiClient mShopApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        final ShopApiClient shopApiClient = new ShopApiClient(APP_ID, APP_PASSWORD, Endpoint.STAGE, new Logger());
-
-        Button categoryTree = (Button) findViewById(R.id.category_tree);
-        categoryTree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shopApiClient.requestCategoryTree(new ApiCallback<CategoryTree>());
-            }
-        });
-
-        Button categories = (Button) findViewById(R.id.categories);
-        categories.setOnClickListener(new View.OnClickListener() {
-            CategoriesRequest mCategoriesRequest = new CategoriesRequest.Builder()
-                    .filterByCategoryIds(Arrays.asList(19534l))
-                    .build();
-
-            @Override
-            public void onClick(View v) {
-                shopApiClient.requestCategories(mCategoriesRequest, new ApiCallback<List<Category>>());
-            }
-        });
-
-        Button facets = (Button) findViewById(R.id.facets);
-        facets.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FacetsRequest facetRequest = new FacetsRequest.Builder()
-                        .filterByFacetGroup(Arrays.asList(FacetGroup.SIZE))
-                        .build();
-
-                shopApiClient.requestFacets(facetRequest, new ApiCallback<List<Facet>>());
-            }
-        });
-
-        Button facetTypes = (Button) findViewById(R.id.facettypes);
-        facetTypes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shopApiClient.requestFacetTypes(new ApiCallback<List<FacetGroup>>());
-            }
-        });
-
-        Button autocomplete = (Button) findViewById(R.id.autocomplete);
-        autocomplete.setOnClickListener(new View.OnClickListener() {
-            AutocompleteRequest autocompleteRequest = new AutocompleteRequest.Builder("Sho")
-                    .filterByTypes( Arrays.asList(Type.PRODUCTS))
-                    .limit(10)
-                    .build();
-
-            @Override
-            public void onClick(View v) {
-                shopApiClient.requestAutocompletion(autocompleteRequest, new ApiCallback<Autocomplete>());
-            }
-        });
-
-        Button productsearch = (Button) findViewById(R.id.productsearch);
-        productsearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Map<FacetGroup, List<Long>> facetFilter = new HashMap<>();
-                facetFilter.put(FacetGroup.CUPSIZE, Arrays.asList(93l, 94l, 95l, 96l));
-
-                ProductSearchRequest request = new ProductSearchRequest.Builder("session4711")
-                        .filterByMinPrice(500)
-                        .filterByMaxPrice(5000)
-                        .filterByStatus(ProductFilter.NONSALEONLY)
-                        .filterByFacets(facetFilter)
-                        .sortBy(Sortby.RELEVANCE)
-                        .sortDirection(Direction.DESC)
-                        .limit(10)
-                        .build();
-
-                shopApiClient.requestProductSearch(request, new ApiCallback<ProductSearch>());
-            }
-        });
-
-        Button products = (Button) findViewById(R.id.products);
-        products.setOnClickListener(new View.OnClickListener() {
-            ProductsRequest productRequest = new ProductsRequest.Builder()
-                    .filterByProductIds(Arrays.asList(329777l, 325136l))
-                    .listFields(Arrays.asList(ProductFields.VARIANTS))
-                    .build();
-
-            @Override
-            public void onClick(View v) {
-                shopApiClient.requestProducts(productRequest, new ApiCallback<List<Product>>());
-            }
-        });
-
-        Button basketadd = (Button) findViewById(R.id.basketadd);
-        basketadd.setOnClickListener(new View.OnClickListener() {
-            BasketAddRequest basketAddRequest = new BasketAddRequest.Builder("session4711")
-                    .setOrderLines(Arrays.asList(new OrderLine(UUID.randomUUID().toString(), 5615651l)))
-                    .build();
-
-            @Override
-            public void onClick(View v) {
-                shopApiClient.requestAddBasket(basketAddRequest, new ApiCallback<Basket>());
-            }
-        });
+        mShopApiClient = new ShopApiClient(APP_ID, APP_PASSWORD, Endpoint.STAGE, new Logger());
     }
-    
+
+    @OnClick(R.id.category_tree)
+    public void categoryTree() {
+        mShopApiClient.requestCategoryTree(new ApiCallback<CategoryTree>());
+    }
+
+    @OnClick(R.id.categories)
+    public void categories() {
+        CategoriesRequest categoriesRequest = new CategoriesRequest.Builder()
+                .filterByCategoryIds(Arrays.asList(19534l))
+                .build();
+
+        mShopApiClient.requestCategories(categoriesRequest, new ApiCallback<List<Category>>());
+    }
+
+    @OnClick(R.id.facets)
+    public void facets() {
+        FacetsRequest facetRequest = new FacetsRequest.Builder()
+                .filterByFacetGroup(Arrays.asList(FacetGroup.SIZE))
+                .build();
+
+        mShopApiClient.requestFacets(facetRequest, new ApiCallback<List<Facet>>());
+    }
+
+    @OnClick(R.id.facettypes)
+    public void facettypes() {
+        mShopApiClient.requestFacetTypes(new ApiCallback<List<FacetGroup>>());
+    }
+
+    @OnClick(R.id.autocomplete)
+    public void autocomplete() {
+        AutocompleteRequest autocompleteRequest = new AutocompleteRequest.Builder("Sho")
+                .filterByTypes( Arrays.asList(Type.PRODUCTS))
+                .limit(10)
+                .build();
+
+
+        mShopApiClient.requestAutocompletion(autocompleteRequest, new ApiCallback<Autocomplete>());
+    }
+
+    @OnClick(R.id.productsearch)
+    public void productsearch() {
+        Map<FacetGroup, List<Long>> facetFilter = new HashMap<>();
+        facetFilter.put(FacetGroup.CUPSIZE, Arrays.asList(93l, 94l, 95l, 96l));
+
+        ProductSearchRequest request = new ProductSearchRequest.Builder("session4711")
+                .filterByMinPrice(500)
+                .filterByMaxPrice(5000)
+                .filterByStatus(ProductFilter.NONSALEONLY)
+                .filterByFacets(facetFilter)
+                .sortBy(Sortby.RELEVANCE)
+                .sortDirection(Direction.DESC)
+                .limit(10)
+                .build();
+
+        mShopApiClient.requestProductSearch(request, new ApiCallback<ProductSearch>());
+    }
+
+    @OnClick(R.id.products)
+    public void products() {
+        ProductsRequest productRequest = new ProductsRequest.Builder()
+                .filterByProductIds(Arrays.asList(329777l, 325136l))
+                .listFields(Arrays.asList(ProductFields.VARIANTS))
+                .build();
+
+        mShopApiClient.requestProducts(productRequest, new ApiCallback<List<Product>>());
+    }
+
+    @OnClick(R.id.basketadd)
+    public void basketadd() {
+        BasketAddRequest basketAddRequest = new BasketAddRequest.Builder("session4711")
+                .setOrderLines(Arrays.asList(new OrderLine(UUID.randomUUID().toString(), 5615651l)))
+                .build();
+
+            mShopApiClient.requestAddBasket(basketAddRequest, new ApiCallback<Basket>());
+    }
+
     private class ApiCallback<T> implements ShopApiClient.Callback<T> {
 
         @Override
