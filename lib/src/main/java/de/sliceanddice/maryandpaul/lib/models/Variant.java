c5f9c9f41d73066d4d0563e638d0db2d57792753
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-14 09:56:38 +0200

    Update variant model

--- lib/src/main/java/de/sliceanddice/maryandpaul/lib/models/Variant.java
+++ lib/src/main/java/de/sliceanddice/maryandpaul/lib/models/Variant.java
@@ -22,44 +22,94 @@ public class Variant {
     private Long retailPrice;
     private Long id;
     private Integer quantity;
+    @SerializedName("default_image")
+    Image image;
 
     public Date getUpdatedDate() {
         return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
     }
 
     public String getEan() {
         return ean;
     }
 
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
     public Boolean getDefaultVariant() {
         return defaultVariant;
+    }
+
+    public void setDefaultVariant(Boolean defaultVariant) {
+        this.defaultVariant = defaultVariant;
     }
 
     public Date getFirstSaleDate() {
         return firstSaleDate;
     }
 
+    public void setFirstSaleDate(Date firstSaleDate) {
+        this.firstSaleDate = firstSaleDate;
+    }
+
     public Long getPrice() {
         return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
     }
 
     public Date getFirstActiveDate() {
         return firstActiveDate;
     }
 
+    public void setFirstActiveDate(Date firstActiveDate) {
+        this.firstActiveDate = firstActiveDate;
+    }
+
     public Long getOldPrice() {
         return oldPrice;
+    }
+
+    public void setOldPrice(Long oldPrice) {
+        this.oldPrice = oldPrice;
     }
 
     public Long getRetailPrice() {
         return retailPrice;
     }
 
+    public void setRetailPrice(Long retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Integer getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
     }
 }
