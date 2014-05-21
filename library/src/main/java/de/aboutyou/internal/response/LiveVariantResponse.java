f5ae48aabefbad6599d2a52e5d469e33146632da
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-21 16:33:03 +0200

    Fix live variants

--- library/src/main/java/de/aboutyou/internal/response/LiveVariantResponse.java
+++ library/src/main/java/de/aboutyou/internal/response/LiveVariantResponse.java
@@ -2,17 +2,19 @@ package de.aboutyou.internal.response;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import de.aboutyou.models.LiveVariant;
 
 public class LiveVariantResponse extends CollinsResponse<List<LiveVariant>> {
 
     @SerializedName("live_variant")
-    private List<LiveVariant> liveVariant;
+    private Map<Long, LiveVariant> liveVariantsResult;
 
     @Override
     public List<LiveVariant> get() {
-        return liveVariant;
+        return new ArrayList<>(liveVariantsResult.values());
     }
 }
