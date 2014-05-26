dde36dfd6500e942f6441272bcd6c041e3f2ab40
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-26 15:59:40 +0200

    Add categories test

---
+++ library/src/test/java/de/aboutyou/CategoriesTest.java
@@ -0,0 +1,71 @@
+package de.aboutyou;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.aboutyou.models.Category;
+import de.aboutyou.request.CategoriesRequest;
+import de.aboutyou.util.MockClient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class CategoriesTest extends TestBase {
+
+    @Test
+    public void testSuccess() {
+        ShopApiClient shopApiClient = getNewApiClient(new SuccessMockClient());
+
+        CategoriesRequest categoriesRequest = new CategoriesRequest.Builder()
+                .filterByCategoryIds(Arrays.asList(1l, 2l))
+                .build();
+
+        List<Category> categories = shopApiClient.requestCategories(categoriesRequest);
+
+        assertNotNull(categories);
+        assertTrue(categories.size() == 2);
+        assertEquals(categories.get(0).getName(), "Cat1");
+    }
+
+    @Test
+    public void testUnknownCategory() {
+        ShopApiClient shopApiClient = getNewApiClient(new FailureMockClient());
+
+        CategoriesRequest categoriesRequest = new CategoriesRequest.Builder()
+                .filterByCategoryIds(Arrays.asList(1l))
+                .build();
+
+        List<Category> categories = shopApiClient.requestCategories(categoriesRequest);
+
+        assertNotNull(categories);
+        assertTrue(categories.size() == 1);
+        assertTrue(categories.get(0).getErrorCode() == 404);
+        assertTrue(categories.get(0).getErrorMessages().size() == 1);
+        assertEquals(categories.get(0).getErrorMessages().get(0), "category not found");
+    }
+
+    private class SuccessMockClient extends MockClient {
+
+        @Override
+        protected void validateRequestBody(String requestBody) {
+            assertEquals(requestBody, "[{\"category\":{\"ids\":[1,2]}}]");
+        }
+
+        @Override
+        protected String getResponse() {
+            return "[{\"category\":{\"1\":{\"active\":true,\"position\":1,\"name\":\"Cat1\",\"parent\":0,\"id\":1},\"2\":{\"active\":true,\"position\":2,\"name\":\"Cat2\",\"parent\":0,\"id\":2}}}]";
+        }
+
+    }
+
+    private class FailureMockClient extends MockClient {
+
+        @Override
+        protected String getResponse() {
+            return "[{\"category\":{\"1\":{\"error_message\":[\"category not found\"],\"error_code\":404}}}]";
+        }
+    }
+}
