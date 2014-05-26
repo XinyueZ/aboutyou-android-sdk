9de955c34a2faafc622f8a0c7d261023adec34ad
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-26 17:13:48 +0200

    Add live variants test

---
+++ library/src/test/java/de/aboutyou/LiveVariantTest.java
@@ -0,0 +1,75 @@
+package de.aboutyou;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.aboutyou.models.Category;
+import de.aboutyou.models.LiveVariant;
+import de.aboutyou.request.CategoriesRequest;
+import de.aboutyou.request.LiveVariantRequest;
+import de.aboutyou.util.MockClient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class LiveVariantTest extends TestBase {
+
+    @Test
+    public void testSuccess() {
+        ShopApiClient shopApiClient = getNewApiClient(new SuccessMockClient());
+
+        LiveVariantRequest liveVariantRequest = new LiveVariantRequest.Builder()
+                .filterByVariantIds(Arrays.asList(1l))
+                .build();
+
+        List<LiveVariant> liveVariants = shopApiClient.requestLiveVariants(liveVariantRequest);
+
+        assertNotNull(liveVariants);
+        assertTrue(liveVariants.size() == 1);
+        assertEquals(liveVariants.get(0).getProductId(), 4711l);
+        assertTrue(liveVariants.get(0).getAvailableStock() == 5);
+        assertTrue(liveVariants.get(0).getPrice() == 1990);
+    }
+
+    @Test
+    public void testVariantNotFound() {
+        ShopApiClient shopApiClient = getNewApiClient(new FailureMockClient());
+
+        LiveVariantRequest liveVariantRequest = new LiveVariantRequest.Builder()
+                .filterByVariantIds(Arrays.asList(2l))
+                .build();
+
+        List<LiveVariant> liveVariants = shopApiClient.requestLiveVariants(liveVariantRequest);
+
+        assertNotNull(liveVariants);
+        assertTrue(liveVariants.size() == 1);
+        assertTrue(liveVariants.get(0).getErrorCode() == 404);
+        assertTrue(liveVariants.get(0).getErrorMessages().size() == 1);
+        assertEquals("Variant not found", liveVariants.get(0).getErrorMessages().get(0));
+    }
+
+    private class SuccessMockClient extends MockClient {
+
+        @Override
+        protected void validateRequestBody(String requestBody) {
+            assertEquals("[{\"live_variant\":{\"ids\":[1]}}]", requestBody);
+        }
+
+        @Override
+        protected String getResponse() {
+            return "[{\"live_variant\":{\"1\":{\"id\":\"1\",\"product_id\":\"4711\",\"available_stock\":\"5\",\"price\":\"1990\"}}}]";
+        }
+
+    }
+
+    private class FailureMockClient extends MockClient {
+
+        @Override
+        protected String getResponse() {
+            return "[{\"live_variant\":{\"2\":{\"error_message\":[\"Variant not found\"],\"error_code\":404,\"id\":2}}}]";
+        }
+    }
+}
