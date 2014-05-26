ef5f0af5bb6e1412cfbc17f6d8a36021b27253a6
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-26 10:14:14 +0200

    Add autocomplete test case

---
+++ library/src/test/java/de/aboutyou/AutocompleteTest.java
@@ -0,0 +1,87 @@
+package de.aboutyou;
+
+import org.junit.Test;
+
+import de.aboutyou.models.Autocomplete;
+import de.aboutyou.request.AutocompleteRequest;
+import de.aboutyou.util.MockClient;
+
+import static org.junit.Assert.*;
+
+public class AutocompleteTest extends TestBase {
+
+    @Test
+    public void testSuccess() {
+        ShopApiClient shopApiClient = getNewApiClient(new SuccessMockClient());
+
+        AutocompleteRequest autocompleteRequest = new AutocompleteRequest.Builder("to").build();
+
+        Autocomplete autocomplete = shopApiClient.requestAutocompletion(autocompleteRequest);
+
+        assertNotNull(autocomplete);
+        assertTrue(autocomplete.getProducts().size() == 1);
+        assertTrue(autocomplete.getCategories().size() == 1);
+    }
+
+    @Test
+    public void testMissingSearchword() {
+        ShopApiClient shopApiClient = getNewApiClient(new MissingSearchwordMockClient());
+
+        AutocompleteRequest autocompleteRequest = new AutocompleteRequest.Builder(null).build();
+
+        Autocomplete autocomplete = shopApiClient.requestAutocompletion(autocompleteRequest);
+
+        assertNotNull(autocomplete);
+        assertTrue(autocomplete.getProducts().size() == 0);
+        assertTrue(autocomplete.getCategories().size() == 0);
+        assertTrue(autocomplete.getErrorCode() == 400);
+        assertTrue(autocomplete.getErrorMessages().size() == 1);
+        assertEquals(autocomplete.getErrorMessages().get(0), ": 'searchword' is required property");
+    }
+
+    @Test
+    public void testShortSearchword() {
+        ShopApiClient shopApiClient = getNewApiClient(new ShortSearchwordMockClient());
+
+        AutocompleteRequest autocompleteRequest = new AutocompleteRequest.Builder("t").build();
+
+        Autocomplete autocomplete = shopApiClient.requestAutocompletion(autocompleteRequest);
+
+        assertNotNull(autocomplete);
+        assertTrue(autocomplete.getProducts().size() == 0);
+        assertTrue(autocomplete.getCategories().size() == 0);
+        assertTrue(autocomplete.getErrorCode() == 400);
+        assertTrue(autocomplete.getErrorMessages().size() == 1);
+        assertEquals(autocomplete.getErrorMessages().get(0), "searchword: u't' is too short");
+    }
+
+    private class SuccessMockClient extends MockClient {
+
+        @Override
+        protected String getResponse() {
+            return "[{\"autocompletion\":{\"products\":[{\"name\":\"Top\",\"id\":1}],\"categories\":[{\"name\":\"Tops\",\"id\":1}]}}]";
+        }
+
+    }
+
+    private class MissingSearchwordMockClient extends MockClient {
+
+        @Override
+        protected String getResponse() {
+            return "[{\"autocompletion\":{\"error_message\":[\": 'searchword' is required property\"],\"error_code\":400}}]";
+        }
+
+    }
+
+    private class ShortSearchwordMockClient extends MockClient {
+
+        @Override
+        protected String getResponse() {
+            return "[{\"autocompletion\":{\"error_message\":[\"searchword: u't' is too short\"],\"error_code\":400}}]";
+        }
+
+    }
+}
+
+
+
