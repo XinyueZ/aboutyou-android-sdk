356a93c21a071208f7105ff8808a1c9dddccc9d1
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-26 15:33:28 +0200

    Add basket tests

---
+++ library/src/test/java/de/aboutyou/BasketModifyTest.java
@@ -0,0 +1,49 @@
+package de.aboutyou;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.aboutyou.models.AddOrderLine;
+import de.aboutyou.models.Basket;
+import de.aboutyou.models.OrderLine;
+import de.aboutyou.request.BasketModifyRequest;
+import de.aboutyou.util.MockClient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class BasketModifyTest extends TestBase {
+
+    @Test
+    public void testSuccess() {
+        ShopApiClient shopApiClient = getNewApiClient(new SuccessMockClient());
+
+        List<OrderLine> orderLines = new ArrayList<>();
+        orderLines.add(new AddOrderLine("id1", 1l));
+
+        BasketModifyRequest basketModifyRequest = new BasketModifyRequest.Builder("foobar")
+                .setOrderLines(orderLines)
+                .build();
+
+        Basket basket = shopApiClient.requestModifyBasket(basketModifyRequest);
+        assertNotNull(basket);
+        assertTrue(basket.getOrderLines().size() == 2);
+    }
+
+    private class SuccessMockClient extends MockClient {
+
+        @Override
+        protected void validateRequestBody(String requestBody) {
+            assertEquals(requestBody, "[{\"basket\":{\"session_id\":\"foobar\",\"order_lines\":[{\"id\":\"id1\",\"variant_id\":1}]}}]");
+        }
+
+        @Override
+        protected String getResponse() {
+            return "[{\"basket\":{\"order_lines\":[{\"id\":\"id1\",\"variant_id\":1},{\"id\":\"id2\",\"variant_id\":2}]}}]";
+        }
+
+    }
+}
--- library/src/test/java/de/aboutyou/util/MockClient.java
+++ library/src/test/java/de/aboutyou/util/MockClient.java
@@ -1,5 +1,6 @@
 package de.aboutyou.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Collections;
 
@@ -7,12 +8,34 @@ import retrofit.client.Client;
 import retrofit.client.Request;
 import retrofit.client.Response;
 import retrofit.mime.TypedByteArray;
+import retrofit.mime.TypedOutput;
 
 public abstract class MockClient implements Client {
 
     @Override
     public Response execute(Request request) throws IOException {
+        validateRequest(request);
         return new Response(request.getUrl(), 200, "no reason", Collections.EMPTY_LIST, new TypedByteArray("application/json", getResponse().getBytes()));
+    }
+
+    private void validateRequest(Request request) {
+        TypedOutput body = request.getBody();
+        if (body.length() == 0) {
+            return;
+        }
+
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            request.getBody(). writeTo(baos);
+            String requestBody = new String(baos.toByteArray(), "UTF-8");
+            validateRequestBody(requestBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void validateRequestBody(String requestBody){
+        // default implementation: do nothing
     }
 
     protected abstract String getResponse();
