04f3af25b4bd66d88d50561a1cec5dadef49f0c9
Henning Dodenhof <henning.dodenhof@4inchworks.de>
2014-05-26 16:41:45 +0200

    Add facettypes test

---
+++ library/src/test/java/de/aboutyou/FacetTypesTest.java
@@ -0,0 +1,38 @@
+package de.aboutyou;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.aboutyou.enums.FacetGroup;
+import de.aboutyou.models.Facet;
+import de.aboutyou.request.FacetsRequest;
+import de.aboutyou.util.MockClient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class FacetTypesTest extends TestBase {
+
+    @Test
+    public void testSuccess() {
+        ShopApiClient shopApiClient = getNewApiClient(new SuccessMockClient());
+
+        List<FacetGroup> facetGroups = shopApiClient.requestFacetTypes();
+
+        assertNotNull(facetGroups);
+        assertTrue(facetGroups.size() == 5);
+        assertEquals(FacetGroup.SIZE, facetGroups.get(2));
+    }
+
+    private class SuccessMockClient extends MockClient {
+
+        @Override
+        protected String getResponse() {
+            return "[{\"facet_types\":[0,1,2,3,4]}]";
+        }
+
+    }
+}
